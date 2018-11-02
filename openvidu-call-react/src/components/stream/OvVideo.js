8782dd213340a88ba8d4cedb7e3767164e37d4d6
Stavros Zavrakas <st.zavrakas@gmail.com>
2018-11-02 15:34:18 +0000

    force-update the video vie

--- openvidu-call-react/src/components/stream/OvVideo.js
+++ openvidu-call-react/src/components/stream/OvVideo.js
@@ -23,13 +23,6 @@ export default class OvVideoComponent extends Component {
         }
     }
 
-    componentDidUpdate() {
-        console.log('componentDidUpdate OvVideoComponent will do forceUpdate');
-        setTimeout(() => {
-            this.forceUpdate();
-        }, 2500);
-    }
-
     render() {
         return (
             <video
