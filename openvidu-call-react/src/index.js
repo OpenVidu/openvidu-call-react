45088ae532122f482fab66e6adbe7e5e44ef4182
shubh <ror.shubh@gmail.com>
2019-07-04 19:35:44 +0530

    removed duplicated files

--- openvidu-call-react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Main from './components/Main';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Main />, document.getElementById('root'));
-registerServiceWorker();
